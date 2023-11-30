7bd1377294de7b3e14333eb87c54df4e0d644983
Yuchuan Han <76800829+agnesyuchuan@users.noreply.github.com>
2023-11-29 22:26:37 -0600

    Add files via upload
    Create RDD and perform Sampling

---
+++ CreateRDD.py
@@ -0,0 +1,16 @@
+from pyspark import SparkContext
+# sampling
+# Create a Spark context
+sc = SparkContext("local", "Sampling Example")
+
+
+# Create an RDD
+data = sc.parallelize(range(1, 100))
+
+
+# Perform sampling
+sampled_rdd = data.sample(withReplacement=False, fraction=0.1, seed=42)
+
+
+# Collect and print the sampled data
+print(sampled_rdd.collect())
